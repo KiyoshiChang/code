3c971f1c9958f7d589ac51688568f327248c6f93
KiyoshiChang <kiyoshihikawa@hotmail.com>
2025-11-20 12:55:22 +0800

    Add files via upload

---
+++ topik/quiz_data.js
@@ -0,0 +1,264 @@
+        // Vocabulary data
+        const vocabularyData = {
+            1: [
+                {korean: '남자 옷', chinese: '男裝', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-15_V01.png'},
+                {korean: '사진', chinese: '照片', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-16_V01.png'},
+                {korean: '걸어요', chinese: '懸掛', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-16_V02.png'},
+                {korean: '가방', chinese: '包包', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V01.png'},
+                {korean: '작은', chinese: '小的', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V02.png'},
+                {korean: '안에', chinese: '在裡面', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V03.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V03.png'}
+            ],
+            2: [
+                {korean: '책상', chinese: '書桌', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V01.png'},
+                {korean: '위에', chinese: '在上面', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V02.png'},
+                {korean: '물', chinese: '水', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V03.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V03.png'},
+                {korean: '닦아요', chinese: '擦拭', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V04.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V04.png'},
+                {korean: '우산', chinese: '雨傘', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V01.png'},
+                {korean: '창문', chinese: '窗戶', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V02.png'}
+            ],
+            3: [
+                {korean: '밑에', chinese: '在下面', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V03.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V03.png'},
+                {korean: '버스', chinese: '公車', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16_V01.png'},
+                {korean: '정류장', chinese: '公車亭', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16_V02.png'},
+                {korean: '배', chinese: '肚子', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15_V01.png'},
+                {korean: '아파요', chinese: '疼痛', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15_V02.png'},
+                {korean: '식탁', chinese: '餐桌', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16_V01.png'}
+            ],
+            4: [
+                {korean: '무거워요', chinese: '沉重的', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16_V02.png'},
+                {korean: '수박', chinese: '西瓜', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V01.png'},
+                {korean: '얼마예요?', chinese: '多少錢?', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V02.png'},
+                {korean: '만', chinese: '一萬', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V03.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V03.png'},
+                {korean: '영화', chinese: '電影', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16_V01.png'},
+                {korean: '들어가요', chinese: '進去', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16_V02.png'}
+            ],
+            5: [
+                {korean: '기다려요', chinese: '等待', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-15_V01.png'},
+                {korean: '짧은', chinese: '短的', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16_V01.png'},
+                {korean: '바지', chinese: '褲子', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16_V02.png'}
+            ]
+        };
+
+        // Practice questions data
+        const practiceQuestions = [
+            {
+                id: 1,
+                vocabularies: [
+                    {korean: '남자 옷', chinese: '男裝', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-15_V01.png'},
+                    {korean: '가방', chinese: '包包', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V01.png'},
+                    {korean: '바지', chinese: '褲子', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16_V02.png'}
+                ]
+            },
+            {
+                id: 2,
+                vocabularies: [
+                    {korean: '사진', chinese: '照片', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-16_V01.png'},
+                    {korean: '걸어요', chinese: '懸掛', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-96st-16_V02.png'},
+                    {korean: '창문', chinese: '窗戶', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V02.png'}
+                ]
+            },
+            {
+                id: 3,
+                vocabularies: [
+                    {korean: '작은', chinese: '小的', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V02.png'},
+                    {korean: '짧은', chinese: '短的', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16_V01.png'},
+                    {korean: '무거워요', chinese: '沉重的', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16_V02.png'}
+                ]
+            },
+            {
+                id: 4,
+                vocabularies: [
+                    {korean: '닦아요', chinese: '擦拭', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V04.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V04.png'},
+                    {korean: '들어가요', chinese: '進去', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16_V02.png'},
+                    {korean: '기다려요', chinese: '等待', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-15_V01.png'}
+                ]
+            },
+            {
+                id: 5,
+                vocabularies: [
+                    {korean: '책상', chinese: '書桌', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V01.png'},
+                    {korean: '식탁', chinese: '餐桌', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16_V01.png'},
+                    {korean: '물', chinese: '水', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V03.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V03.png'}
+                ]
+            },
+            {
+                id: 6,
+                vocabularies: [
+                    {korean: '안에', chinese: '在裡面', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V03.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15_V03.png'},
+                    {korean: '위에', chinese: '在上面', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16_V02.png'},
+                    {korean: '밑에', chinese: '在下面', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V03.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V03.png'}
+                ]
+            },
+            {
+                id: 7,
+                vocabularies: [
+                    {korean: '우산', chinese: '雨傘', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15_V01.png'},
+                    {korean: '배', chinese: '肚子', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15_V01.png'},
+                    {korean: '아파요', chinese: '疼痛', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15_V02.png'}
+                ]
+            },
+            {
+                id: 8,
+                vocabularies: [
+                    {korean: '버스', chinese: '公車', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16_V01.png'},
+                    {korean: '정류장', chinese: '公車亭', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16_V02.png'},
+                    {korean: '영화', chinese: '電影', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16_V01.png'}
+                ]
+            },
+            {
+                id: 9,
+                vocabularies: [
+                    {korean: '수박', chinese: '西瓜', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V01.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V01.png'},
+                    {korean: '얼마예요?', chinese: '多少錢?', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V02.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V02.png'},
+                    {korean: '만', chinese: '一萬', audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V03.mp3', image: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15_V03.png'}
+                ]
+            }
+        ];
+
+        // Questions data
+        const questionsData = [
+            {
+                id: 1,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-96th-15.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-96th-15.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-96th-15a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-96th-15b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-96th-15c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-96th-15d.png'
+                ],
+                correct: 0
+            },
+            {
+                id: 2,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-96th-16.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-96th-16.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-96th-16a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-96th-16b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-96th-16c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-96th-16d.png'
+                ],
+                correct: 0
+            },
+            {
+                id: 3,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-91st-15.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-91st-15d.png'
+                ],
+                correct: 0
+            },
+            {
+                id: 4,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-91st-16.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-91st-16d.png'
+                ],
+                correct: 0
+            },
+            {
+                id: 5,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-83rd-15.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-15d.png'
+                ],
+                correct: 0
+            },
+            {
+                id: 6,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-83rd-16.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-83rd-16d.png'
+                ],
+                correct: 3
+            },
+            {
+                id: 7,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-64th-15.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-64th-15d.png'
+                ],
+                correct: 2
+            },
+            {
+                id: 8,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-64th-16.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-64th-16d.png'
+                ],
+                correct: 0
+            },
+            {
+                id: 9,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-60th-15.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-60th-15d.png'
+                ],
+                correct: 0
+            },
+            {
+                id: 10,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-60th-16.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-60th-16d.png'
+                ],
+                correct: 0
+            },
+            {
+                id: 11,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-15.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-47th-15.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-47th-15a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-47th-15b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-47th-15c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-47th-15d.png'
+                ],
+                correct: 0
+            },
+            {
+                id: 12,
+                audio: 'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16.mp3',
+                vtt: 'https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-47th-16.vtt',
+                images: [
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16a.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16b.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16c.png',
+                    'https://mon-ami.up.seesaa.net/topik/Topik1-47th-16d.png'
+                ],
+                correct: 0
+            }
+        ];
