cee2be96655f9c7ed55a6f51f39a0a84518a8df4
KiyoshiChang <kiyoshihikawa@hotmail.com>
2025-12-06 20:30:53 +0800

    Add files via upload

---
+++ topik/quiz_data01b.js
@@ -0,0 +1,119 @@
+        // *** MODIFICATION: Fixed all broken https links ***
+        const questions = [
+            {
+                audio: "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q19.mp3",
+                subtitle: "https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-Listening_Q19.vtt",
+                options: [
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q19a.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q19b.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q19c.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q19d.jpg"
+                ],
+                correct: 0
+            },
+            {
+                audio: "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q20.mp3",
+                subtitle: "https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-Listening_Q20.vtt",
+                options: [
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q20a.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q20b.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q20c.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q20d.jpg"
+                ],
+                correct: 0
+            },
+            {
+                audio: "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q06.mp3",
+                subtitle: "https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-Listening_Q06.vtt",
+                options: [
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q06a.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q06b.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q06c.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q06d.jpg"
+                ],
+                correct: 0
+            },
+            {
+                audio: "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q08.mp3",
+                subtitle: "https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-Listening_Q08.vtt",
+                options: [
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q08a.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q08b.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q08c.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q08d.jpg"
+                ],
+                correct: 0
+            },
+            {
+                audio: "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q09.mp3",
+                subtitle: "https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-Listening_Q09.vtt",
+                options: [
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q09a.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q09b.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q09c.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q09d.jpg"
+                ],
+                correct: 0
+            },
+
+            {
+                audio: "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q11.mp3",
+                subtitle: "https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-Listening_Q11.vtt",
+                options: [
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q11a.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q11b.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q11c.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q11d.jpg"
+                ],
+                correct: 0
+            },
+
+            {
+                audio: "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q13.mp3",
+                subtitle: "https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-Listening_Q13.vtt",
+                options: [
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q13a.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q13b.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q13c.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q13d.jpg"
+                ],
+                correct: 0
+            },
+
+            {
+                audio: "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q15.mp3",
+                subtitle: "https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-Listening_Q15.vtt",
+                options: [
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q15a.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q15b.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q15c.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q15d.jpg"
+                ],
+                correct: 0
+            },
+
+            {
+                audio: "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q17.mp3",
+                subtitle: "https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-Listening_Q17.vtt",
+                options: [
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q17a.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q17b.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q17c.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q17d.jpg"
+                ],
+                correct: 0
+            },
+
+            {
+                audio: "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q18.mp3",
+                subtitle: "https://kiyoshichang.github.io/cassette/vtt_topik/Topik1-Listening_Q18.vtt",
+                options: [
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q18a.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q18b.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q18c.jpg",
+                    "https://mon-ami.up.seesaa.net/topik/Topik1-Listening_Q18d.jpg"
+                ],
+                correct: 0
+            }
+
+        ];
